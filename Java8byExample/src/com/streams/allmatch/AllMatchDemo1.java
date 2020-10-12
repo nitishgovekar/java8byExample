a2419e834ef198bc334732539d09bad5b42b2d7d
nitish.govekar <nitishgovekar1992@gmail.com>
2020-10-12 21:13:35 +0530

    Updated code to add examples related to allmatch() ==>JAVA8 Streams

---
+++ Java8byExample/src/com/streams/allmatch/AllMatchDemo1.java
@@ -0,0 +1,17 @@
+package com.streams.allmatch;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AllMatchDemo1 {
+
+	public static void main(String[] args) {
+
+		List<String> list = Arrays.asList("Virat","Rohit","Dhoni");
+		//returns whether all elements of this stream match the provided predicate
+		boolean result = list.stream().allMatch(s ->s.contains("i"));
+
+		System.out.println(result);
+	} 
+
+}
---
+++ Java8byExample/src/com/streams/allmatch/AllMatchDemo2.java
@@ -0,0 +1,17 @@
+package com.streams.allmatch;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AllMatchDemo2 {
+
+	public static void main(String[] args) {
+
+		List<String> list =Arrays.asList("Virat", "Rohit" , "Raina" ,"Dhoni");
+		//Checks whether the first character is in UpperCase or Not
+		boolean result = list.stream().allMatch(s->Character.isUpperCase(s.charAt(0)));
+		System.out.println(result);
+		
+	}
+
+}
